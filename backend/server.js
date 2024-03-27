24f60df745d6142b50c343355ae250ae30ab0361
alqamah <alqama.hasnain@gmail.com>
2024-03-27 11:37:40 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import server from "./app.js";
 import { connectDB } from "./config/db.js";
 
-const serverStar = server.listen(process.env.PORT, async (err) => {
+
+
+const serverStart = server.listen(process.env.PORT, async (err) => {
   if (err) {
     console.log(`server failed with error ${err}`);
   } else {

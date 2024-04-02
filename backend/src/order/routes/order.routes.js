699345326a7368d8ee9698b14c7ac705b141c196
alqamah <alqama.hasnain@gmail.com>
2024-04-02 11:30:09 +0530

    order controller and repo added (not tested)

--- backend/src/order/routes/order.routes.js
+++ backend/src/order/routes/order.routes.js
@@ -3,7 +3,6 @@ import { createNewOrder } from "../controllers/order.controller.js";
 import { auth } from "../../../middlewares/auth.js";
 
 const router = express.Router();
-
 router.route("/new").post(auth, createNewOrder);
 
 export default router;
